4df101a2ebeafa04e80edaa959b27ddb86b04ab9
xingyue <qixingyue@126.com>
2016-01-27 16:15:43 +0800

    释放以后，赋值为NULL

--- mempool/pool.h
+++ mempool/pool.h
@@ -20,12 +20,17 @@
 #define ALIGN_SIZE (sizeof(void*) - 1)
 
 #define POOL_RUN(max,handler) \
-	_assert(NULL == pool , "YOU MUST END THE LAST POOL_RUN\n" ) ; \
+	_assert(NULL != pool , "YOU MUST END THE LAST POOL_RUN\n" ) ; \
 	pool = pool_new(max,handler); 
-#define POOL_RUN_END() pool_destroy(pool);
+#define POOL_RUN_END() pool_destroy(pool); pool = NULL;
 
 typedef struct pool pool_t;
 typedef void (*over_size_t_handler)(size_t);
+
+typedef struct {
+	void *(*malloc)(size_t);
+	void (*free)(void*);
+} memsys;
 
 struct block {
 	struct block *next;
@@ -40,11 +45,15 @@ struct pool {
 	over_size_t_handler too_much_handler;
 };
 
+
 pool_t * pool_new(size_t,over_size_t_handler);
 void pool_destroy(pool_t *);
 void * pool_malloc(pool_t *, size_t );
 static void default_too_much_hanlder(size_t next_alloc);
 
+static memsys default_mem_sys = {
+	malloc,free
+};
 pool_t *pool;
 
 #endif
